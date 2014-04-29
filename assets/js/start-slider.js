050e9a6b655498f8accf6f76976ad0227f5634a6
Chris Mumford <mumfordnsons@gmail.com>
2014-04-29 20:18:22 +0100

    Adding mumf-slider jQuery plugin.

---
+++ assets/js/start-slider.js
@@ -0,0 +1,13 @@
+(function ($) {
+
+
+	// Load mumf slider.
+	$('.mumf-slider').mumfSlider({
+	    transition: 'fade',
+	    transitionSpeed: 150
+	});
+
+	console.log($('.mumf-slider'))
+
+}(jQuery));
+
